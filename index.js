391e7af23b696c9703ccc0117254189f8c459a93
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-24 03:13:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -15,13 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
